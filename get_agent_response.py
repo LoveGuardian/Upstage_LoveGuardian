ba3cedebbcb6dcbdb049b546b301376e6d2c1cf7
Minjoon Choi <miles@dunamu.com>
2024-05-18 15:20:12 +0900

    store & use previous conversations per agent

--- get_agent_response.py
+++ get_agent_response.py
@@ -1,44 +1,58 @@
 import streamlit as st
-import os
-
-from langchain.chains import LLMChain
-from langchain.llms import OpenAI
 
 from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from langchain_upstage import ChatUpstage
-from langchain_community.document_loaders import TextLoader
 
 from agent_selection_page import AGENT_DESCRIPTIONS
 
 
-
-
 def get_agent_response(agent, user_input, chat_data):
-
-    llm = ChatUpstage(api_key = st.session_state.solar_api_key)
+    llm = ChatUpstage(api_key=st.session_state.solar_api_key)
 
     prompt_template = PromptTemplate.from_template(
         """
-        Please provide most relevant response based on the following context which is a text of chat between two possible lovers.
-        You're {agent_type} with "{agent_personality}" personality.   
-        ---
-        Question: {question}
-        ---
-        Context: {Context}
+        Please provide the most relevant response based on the given context.
+        
+        First, a text of chat between two possible lovers, is given as "<Chat>".
+        Second, the previous questions and the corresponding responses given by you, are given as "<Conversation>".
+        Third, the question on which you should provide a response now, is given as "<Question>".
+        Each region is separated by "-----------".
+        
+        Keep in mind that you are "{agent_type}" agent.
+        You should respond based on a "{agent_personality}" perspective.
+        
+        ----------
+        <Chat>
+        {chat}
+        ----------
+        <Conversation>
+        {conversation}
+        ----------
+        <Question>
+        {question}
         """
     )
     chain = prompt_template | llm | StrOutputParser()
+
     # 3. define chain
     agent_type = agent
     agent_personality = AGENT_DESCRIPTIONS[agent]
 
-    # chain.invoke({"agent_type": agent_type, "agent_personality": agent_personality, "question": "What do you think is the personality of the guy?", "Context": docs})
+    response = chain.invoke(
+        {
+            "agent_type": agent_type,
+            "agent_personality": agent_personality,
+            "chat": chat_data,
+            "conversation": st.session_state.conversation_history.get(agent, ""),
+            "question": user_input,
+        }
+    )
 
-    # chain.invoke({"agent_type": agent_type, "agent_personality": agent_personality, "question": "Describe the current relationship status of these two people", "Context": docs})
-
-    # chain.invoke({"agent_type": agent_type, "agent_personality": agent_personality, "question": "Do you think the guy is interested in me?", "Context": docs})
-
-    response = chain.invoke({"agent_type": agent_type, "agent_personality": agent_personality, "question": user_input, "Context": chat_data})
+    st.session_state.conversation_history[agent] = (
+            st.session_state.conversation_history.get(agent, "")
+            + f"Q: {user_input}\n"
+            + f"A: {response}\n\n"
+    )
 
     return response
