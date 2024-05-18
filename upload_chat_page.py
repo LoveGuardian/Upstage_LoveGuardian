6dbfedc798feed07e1e6c8077311f176e9cb4a5c
jh.michael.shin <jh.michael.shin@gmail.com>
2024-05-18 02:41:19 +0000

    add csv format

--- upload_chat_page.py
+++ upload_chat_page.py
@@ -1,12 +1,18 @@
 import streamlit as st
-
+import pandas as pd
 
 def upload_chat_page():
     st.title("Upload Your Chat")
-    st.write("Upload your KakaoTalk chat file in .txt format.")
+    st.write("Upload your KakaoTalk chat file in .txt or csv format.")
+    uploaded_file = st.file_uploader("Choose a file...", type=["txt", "csv"])
 
-    uploaded_file = st.file_uploader("Choose a file...", type="txt")
+    # I need to upload a KakaoTalk chat file in .csv format as well
     if uploaded_file:
-        st.session_state.chat_data = uploaded_file.read().decode("utf-8")
+        # if uploaded_file.type is txt
+        if uploaded_file.type == "txt":
+            st.session_state.chat_data = uploaded_file.read().decode("utf-8")
+        elif uploaded_file.type == "csv":
+            chat_data = pd.read_csv(uploaded_file)
+            st.session_state.chat_data = chat_data.to_string(index=False)
         if st.button("Upload"):
             st.session_state.page = "chatting_page"
